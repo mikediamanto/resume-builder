76584ece606eff13664a9c0dad5949232d450f80
Mike Diamantopoulos <mdiamantopoulos@novibet.com>
2024-12-07 16:16:52 +0200

    usesearchparams fix

--- src/app/(main)/editor/resume-editor.tsx
+++ src/app/(main)/editor/resume-editor.tsx
@@ -4,7 +4,7 @@ import { useSearchParams } from "next/navigation";
 import { steps } from "./steps";
 import Breadcrumbs from "./breadcrumbs";
 import Footer from "./footer";
-import { useState } from "react";
+import { Suspense, useState } from "react";
 import { ResumeValues } from "@/lib/validation";
 import ResumePreviewSection from "./resume-preview-section";
 import { cn } from "@/lib/utils";
@@ -29,45 +29,47 @@ const ResumeEditor = () => {
   )?.component;
 
   return (
-    <div className="flex grow flex-col">
-      <header className="space-y-1.5 border-b px-3 py-5 text-center">
-        <h1 className="text-2xl font-bold">Design your resume</h1>
-        <p className="text-sm text-muted-foreground">
-          Follow the steps below to create your resume. Your progress will be
-          save automatically.
-        </p>
-      </header>
-      <main className="relative grow">
-        <div className="absolute bottom-0 top-0 flex w-full">
-          <div
-            className={cn(
-              "w-full space-y-6 overflow-y-auto p-3 md:block md:w-1/2",
-              showSmResumePreview && "hidden",
-            )}
-          >
-            <Breadcrumbs currentStep={currentStep} setCurrentStep={setStep} />
-            {FormComponent && (
-              <FormComponent
-                resumeData={resumeData}
-                setResumeData={setResumeData}
-              />
-            )}
+    <Suspense>
+      <div className="flex grow flex-col">
+        <header className="space-y-1.5 border-b px-3 py-5 text-center">
+          <h1 className="text-2xl font-bold">Design your resume</h1>
+          <p className="text-sm text-muted-foreground">
+            Follow the steps below to create your resume. Your progress will be
+            save automatically.
+          </p>
+        </header>
+        <main className="relative grow">
+          <div className="absolute bottom-0 top-0 flex w-full">
+            <div
+              className={cn(
+                "w-full space-y-6 overflow-y-auto p-3 md:block md:w-1/2",
+                showSmResumePreview && "hidden",
+              )}
+            >
+              <Breadcrumbs currentStep={currentStep} setCurrentStep={setStep} />
+              {FormComponent && (
+                <FormComponent
+                  resumeData={resumeData}
+                  setResumeData={setResumeData}
+                />
+              )}
+            </div>
+            <div className="grow md:border-r" />
+            <ResumePreviewSection
+              resumeData={resumeData}
+              setResumeData={setResumeData}
+              className={cn(showSmResumePreview && "flex")}
+            />
           </div>
-          <div className="grow md:border-r" />
-          <ResumePreviewSection
-            resumeData={resumeData}
-            setResumeData={setResumeData}
-            className={cn(showSmResumePreview && "flex")}
-          />
-        </div>
-      </main>
-      <Footer
-        currentStep={currentStep}
-        setCurrentStep={setStep}
-        showSmResumePreview={showSmResumePreview}
-        setShowSmResumePreview={setShowSmResumePreview}
-      />
-    </div>
+        </main>
+        <Footer
+          currentStep={currentStep}
+          setCurrentStep={setStep}
+          showSmResumePreview={showSmResumePreview}
+          setShowSmResumePreview={setShowSmResumePreview}
+        />
+      </div>
+    </Suspense>
   );
 };
 
