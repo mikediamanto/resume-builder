1eac546924b928c488b2f70cc9a21b29b38dcc7b
Mike Diamantopoulos <mdiamantopoulos@novibet.com>
2024-12-07 16:26:39 +0200

    added redirect

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  redirects: async () => [
+    {
+      source: "/",
+      destination: "/resumes",
+      statusCode: 302,
+    },
+  ],
 };
 
 export default nextConfig;
